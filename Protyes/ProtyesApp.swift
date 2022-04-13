a19b246006453e72880c6e27869cf64277a22ce7
SuadWarsame <suad.warsame@city.ac.uk>
2022-04-13 23:36:39 +0100

    Initial Commit

---
+++ Protyes/ProtyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtyesApp.swift
+//  Protyes
+//
+//  Created by Suad Warsame on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProtyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
