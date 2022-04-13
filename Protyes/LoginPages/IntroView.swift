acb7e1c1712d46b0373821b8cfd3f76d1063c4fc
SuadWarsame <suad.warsame@city.ac.uk>
2022-04-13 23:47:09 +0100

    Login file commit

---
+++ Protyes/LoginPages/IntroView.swift
@@ -0,0 +1,20 @@
+//
+//  IntroView.swift
+//  Protyes
+//
+//  Created by Suad Warsame on 13/04/2022.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
