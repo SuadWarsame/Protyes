aba92306c0200d1ddf7e760dd80b761c5f641b11
SuadWarsame <suad.warsame@city.ac.uk>
2022-04-13 23:58:59 +0100

    Added files

---
+++ Protyes/LoginPages/AdminLogin.swift
@@ -0,0 +1,20 @@
+//
+//  AdminLogin.swift
+//  Protyes
+//
+//  Created by Suad Warsame on 13/04/2022.
+//
+
+import SwiftUI
+
+struct AdminLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdminLogin_Previews: PreviewProvider {
+    static var previews: some View {
+        AdminLogin()
+    }
+}
---
+++ Protyes/LoginPages/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Protyes
+//
+//  Created by Suad Warsame on 13/04/2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
